c842f72add34d8dbdc95bf975fda81002047034c
IlyaKha <powerfulkha96@mail.ru>
2024-05-28 15:28:59 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -18,6 +18,6 @@ result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
 
-print(result1)  # [[10, 10], [10, 10]]
-print(result2)  # [[42, 42, 42, 42, 42], [42, 42, 42, 42, 42], [42, 42, 42, 42, 42]]
-print(result3)  # [[13, 13], [13, 13], [13, 13], [13, 13]]
+print(result1) 
+print(result2)  
+print(result3)  
