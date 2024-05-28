9e200b4548952e59e3b5a0fb648e3e9ada8e7f08
IlyaKha <powerfulkha96@mail.ru>
2024-05-28 15:27:47 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,23 @@
+
+
+
+def get_matrix(n, m, value):
+    matrix = []
+    if n <= 0 or m <= 0:
+        return matrix  # Возвращает пустой список если n или m < или = 0
+
+    for _ in range(n):
+        matrix.append([])
+        for _ in range(m):
+            matrix[-1].append(value)
+
+    return matrix
+
+# Примеры
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+
+print(result1)  # [[10, 10], [10, 10]]
+print(result2)  # [[42, 42, 42, 42, 42], [42, 42, 42, 42, 42], [42, 42, 42, 42, 42]]
+print(result3)  # [[13, 13], [13, 13], [13, 13], [13, 13]]
